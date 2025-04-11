27afeeaa606dec18623d4a36439e0a8b0894c51b
DBaptista <duarte.baptista@tecnico.ulisboa.pt>
2025-04-11 18:31:42 +0100

    Resource to pass state of operations, still review if the Success with offline flag is the best option

--- app/src/main/java/com/example/catsapisampleproject/util/Resource.kt
+++ app/src/main/java/com/example/catsapisampleproject/util/Resource.kt
@@ -1,10 +1,11 @@
 package com.example.catsapisampleproject.util
 
 /**
- * class to send data with the correct state (success, error, loading)
+ * class to send data with the correct state (success (offline flag to inform the user), error
+ * , loading)
  */
 sealed class Resource<T>(val data: T? = null, val uiText: String? = null) {
-    class Success<T>(data: T?): Resource<T>(data)
+    class Success<T>(data: T?, offline: Boolean = false): Resource<T>(data)
     class Error<T>(uiText: String, data: T? = null): Resource<T>(data, uiText)
     class Loading<T>(data: T? = null): Resource<T>(data)
 }
