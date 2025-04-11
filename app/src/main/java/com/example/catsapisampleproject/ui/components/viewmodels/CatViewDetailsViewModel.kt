d20998ea84946c42df9ca710a0ec8240b09c74f5
DBaptista <duarte.baptista@tecnico.ulisboa.pt>
2025-04-11 18:27:17 +0100

    Show breed details screen and viewModel. BreedID passed by nav argument

---
+++ app/src/main/java/com/example/catsapisampleproject/ui/components/viewmodels/CatViewDetailsViewModel.kt
@@ -0,0 +1,138 @@
+package com.example.catsapisampleproject.ui.components.viewmodels
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.catsapisampleproject.dataLayer.repositories.BreedWithImage
+import com.example.catsapisampleproject.domain.useCases.DeleteCatFavouriteUseCase
+import com.example.catsapisampleproject.domain.useCases.GetCatBreedUseCase
+import com.example.catsapisampleproject.domain.useCases.SetCatFavouriteUseCase
+import com.example.catsapisampleproject.util.Resource
+import com.example.catsapisampleproject.util.StringUtils
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+data class CatDetailsUIState(
+    val breed: BreedWithImage? = null,
+    val isLoading: Boolean = false,
+    val error: String = "",
+    val favouriteOperationError: String = ""
+)
+
+@HiltViewModel
+class CatViewDetailsViewModel @Inject constructor(
+    savedStateHandle : SavedStateHandle,
+    private val getCatBreedDetailsUseCase: GetCatBreedUseCase,
+    private val setCatFavouriteUseCase: SetCatFavouriteUseCase,
+    private val deleteCatFavouriteUseCase: DeleteCatFavouriteUseCase
+) : ViewModel() {
+
+    // retrieve breedId from navigation arguments
+    private val breedId = savedStateHandle.get<String>("breedId")
+
+    private val _uiState = MutableStateFlow(CatDetailsUIState())
+    val uiState = _uiState.asStateFlow()
+
+    init {
+        if (breedId != null) {
+            loadBreedDetails(breedId)
+        } else {
+            _uiState.value = CatDetailsUIState(isLoading = false, error = "Breed ID missing")
+        }
+    }
+
+    private fun loadBreedDetails(breedId: String) {
+        getCatBreedDetailsUseCase.invoke(breedId).onEach { result ->
+            when (result) {
+                is Resource.Success -> {
+                    _uiState.update {
+                        it.copy(
+                            isLoading = false,
+                            breed = result.data,
+                            error = StringUtils.EMPTY_STRING
+                        )
+                    }
+                }
+                is Resource.Error -> {
+                    _uiState.update {
+                        it.copy(
+                            error = result.uiText ?: "An unexpected error occurred",
+                            isLoading = false
+                        )
+                    }
+                }
+                is Resource.Loading -> _uiState.update { it.copy(isLoading = true) }
+            }
+        }.launchIn(viewModelScope)
+    }
+
+    // Handle the favorite button click -> delegate to the appropriate use case
+    fun toggleFavourite() {
+        viewModelScope.launch {
+            val currentState = _uiState.value
+            val refId = currentState.breed?.image?.image_id ?: return@launch
+
+            if (currentState.breed.isFavourite) {
+                // Remove favourite.
+                deleteCatFavouriteUseCase.invoke(refId)
+                    .onEach { result ->
+                        when (result) {
+                            is Resource.Error -> {
+                                _uiState.update {
+                                    it.copy(
+                                        favouriteOperationError = result.uiText ?: "An unexpected error occurred",
+                                        isLoading = false
+                                    )
+                                }
+                            }
+
+                            is Resource.Loading -> _uiState.update { it.copy(isLoading = true) }
+                            is Resource.Success -> {
+                                _uiState.update {
+                                    it.copy(
+                                        isLoading = false,
+                                        error = StringUtils.EMPTY_STRING,
+                                        breed = it.breed?.copy(isFavourite = false)
+                                    )
+                                }
+                            }
+                        }
+                    }
+                    .launchIn(viewModelScope)
+            } else {
+                // Set favourite.
+                setCatFavouriteUseCase.invoke(refId)
+                    .onEach { result ->
+                        when (result) {
+                            is Resource.Error -> {
+                                _uiState.update {
+                                    it.copy(
+                                        favouriteOperationError = result.uiText ?: "An unexpected error occurred",
+                                        isLoading = false
+                                    )
+                                }
+                            }
+
+                            is Resource.Loading -> _uiState.update { it.copy(isLoading = true) }
+                            is Resource.Success -> {
+                                _uiState.update {
+                                    it.copy(
+                                        isLoading = false,
+                                        favouriteOperationError = StringUtils.EMPTY_STRING,
+                                        breed = it.breed?.copy(isFavourite = true)
+                                    )
+                                }
+                            }
+                        }
+                    }
+                    .launchIn(viewModelScope)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/catsapisampleproject/ui/screens/DetailsScreen.kt
@@ -0,0 +1,168 @@
+package com.example.catsapisampleproject.ui.screens
+
+import android.widget.Toast
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
+import com.bumptech.glide.integration.compose.placeholder
+import com.example.catsapisampleproject.R
+import com.example.catsapisampleproject.dataLayer.repositories.BreedWithImage
+import com.example.catsapisampleproject.domain.model.CatBreed
+import com.example.catsapisampleproject.domain.model.CatBreedImage
+import com.example.catsapisampleproject.ui.components.viewmodels.CatDetailsUIState
+import com.example.catsapisampleproject.ui.components.viewmodels.CatViewDetailsViewModel
+import com.example.catsapisampleproject.ui.misc.FullScreenLoadingOverlay
+
+@Composable
+fun CatDetailsScreen(
+    breedId: String?,
+    viewModel: CatViewDetailsViewModel = hiltViewModel()  // SavedStateHandle provides the breedId
+) {
+    val uiState by viewModel.uiState.collectAsState()
+
+    CatDetailsScreenContent(
+        uiState,
+        onFavoriteClick = { viewModel.toggleFavourite() }
+    )
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalGlideComposeApi::class)
+@Composable
+fun CatDetailsScreenContent(
+    uiState: CatDetailsUIState,
+    onFavoriteClick: () -> Unit,
+) {
+    val context = LocalContext.current
+
+    // Show toast if there's a favorite operation error
+    LaunchedEffect(key1 = uiState.favouriteOperationError) {
+        if (uiState.favouriteOperationError.isNotEmpty()) {
+            Toast.makeText(context, uiState.favouriteOperationError, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        if (uiState.error.isNotEmpty()) {
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                Text(text = uiState.error)
+            }
+        } else {
+            uiState.breed?.let { breed ->
+                Scaffold(
+                    topBar = {
+                        TopAppBar(title = { Text(breed.breed.name) })
+                    }
+                ) { padding ->
+                    Column(
+                        modifier = Modifier
+                            .padding(padding)
+                            .fillMaxSize()
+                            .padding(16.dp),
+                    ) {
+                        GlideImage(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .weight(1f),
+                            model = breed.image?.url,
+                            loading = placeholder(R.drawable.ic_generic_cat_drawable),
+                            failure = placeholder(R.drawable.ic_generic_cat_drawable),
+                            contentScale = ContentScale.Fit,
+                            contentDescription = breed.breed.name + " Image"
+                        )
+                        Column {
+                            Spacer(modifier = Modifier.height(16.dp))
+                            Text(
+                                text = "Origin: ${breed.breed.origin}",
+                                style = MaterialTheme.typography.bodyLarge
+                            )
+                            Spacer(modifier = Modifier.height(8.dp))
+                            Text(
+                                text = "Temperament: ${breed.breed.temperament}",
+                                style = MaterialTheme.typography.bodyLarge
+                            )
+                            Spacer(modifier = Modifier.height(8.dp))
+                            Text(
+                                text = "Description: ${breed.breed.description}",
+                                style = MaterialTheme.typography.bodyLarge
+                            )
+                            Spacer(modifier = Modifier.height(16.dp))
+
+                            Button(
+                                onClick = { onFavoriteClick() },
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .align(Alignment.End),
+                                colors = if (breed.isFavourite) {
+                                    ButtonDefaults.buttonColors(
+                                        containerColor = Color.Red
+                                    )
+                                } else {
+                                    ButtonDefaults.buttonColors(
+                                        containerColor = Color.Green,
+                                    )
+                                }
+                            ) {
+                                Text(text = if (breed.isFavourite) "Remove from Favorites" else "Add to Favorites")
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        // Show full screen loading only when loading the breed details
+        if (uiState.isLoading) {
+            FullScreenLoadingOverlay()
+        }
+
+    }
+}
+
+@Composable
+@Preview
+fun CatDetailsScreenContentPreview() {
+    CatDetailsScreenContent(
+        uiState = CatDetailsUIState(
+            breed = BreedWithImage(
+                breed = CatBreed(
+                    id = "id",
+                    name = "name",
+                    description = "desc",
+                    temperament = "temperament",
+                    origin = "origin",
+                    referenceImageId = "refImgId",
+                    minLifeSpan = 1,
+                    maxLifeSpan = 1
+                ),
+                image = CatBreedImage(
+                    image_id = "AAAAA",
+                    url = "https://cdn2.thecatapi.com/images/0SxW2SQ_S.jpg",
+                    breed_id = "id",
+                ),
+                isFavourite = false
+            ),
+            isLoading = false,
+            error = ""
+        ),
+        onFavoriteClick = {}
+    )
+}
+
